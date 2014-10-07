6aa069d3ae6e50572885837c721f2cc7d72797d2
lmaisour <lmaisour@gmail.com>
2014-10-07 16:52:08 -0700

    modified sessions controller to work on heroku?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,22 @@ class SessionsController < ApplicationController
     redirect_to '/auth/github'
   end
 
-  def create
-    @user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
-    if @user
-         session[:user_id] = @user.id
-         redirect_to rooms_path
-    else
-         redirect_to root_url
-    end
-  end
-
-  # def create     
-  #   auth = request.env["omniauth.auth"]     
-  #   user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
-  #   session[:user_id] = user.id     
-  #   redirect_to root_url
+  # def create
+  #   @user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
+  #   if @user
+  #        session[:user_id] = @user.id
+  #        redirect_to rooms_path
+  #   else
+  #        redirect_to root_url
+  #   end
   # end
+
+  def create     
+    auth = request.env["omniauth.auth"]     
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
+    session[:user_id] = user.id     
+    redirect_to root_url
+  end
 
   def destroy
     reset_session
