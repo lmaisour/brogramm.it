b582d9ef60dba80ae7db6b651e53277ab3cfdc85
lmaisour <lmaisour@gmail.com>
2014-10-07 17:14:22 -0700

    heroku not working because of gitignore file containing github env var

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,27 @@
 class SessionsController < ApplicationController
-
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
   def new
     redirect_to '/auth/github'
   end
 
-  # def create
-  #   @user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
-  #   if @user
-  #        session[:user_id] = @user.id
-  #        redirect_to rooms_path
-  #   else
-  #        redirect_to root_url
-  #   end
-  # end
-
-  def create     
-    auth = request.env["omniauth.auth"]     
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
-    session[:user_id] = user.id     
-    redirect_to root_url
+  def create
+    @user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
+    if @user
+         session[:user_id] = @user.id
+         redirect_to rooms_path
+    else
+         redirect_to root_url
+    end
   end
+
+  # def create     
+  #   auth = request.env["omniauth.auth"]     
+  #   user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
+  #   session[:user_id] = user.id     
+  #   redirect_to root_url
+  # end
 
   def destroy
     reset_session
